a350dfae757013ca6004989cdd16fc1ed070b560
Tusa05 <nataliyazayac16@gmail.com>
2025-11-03 21:55:08 +0300

    Update config.py

--- kinopoisk_autotests_diploma/config.py
+++ kinopoisk_autotests_diploma/config.py
@@ -2,12 +2,12 @@ class Config:
     # Основные настройки окружения
     BASE_URL = "https://www.kinopoisk.ru/"  # URL сайта
     API_BASE_URL = "https://api.kinopoisk.dev/"  # Замените на URL API
-    
+
     # Данные пользователя
-    USER_EMAIL = ""  # email
-    USER_PASSWORD = ""  # Пароль
+    USER_EMAIL = "nataliyazayac2009@mail.ru"  # email
+    USER_PASSWORD = "123456Q_w"  # Пароль
     # Токен для доступа к API
-    AUTH_TOKEN = ""  # Токен API
+    AUTH_TOKEN = "CCJBXJ1-QQVMY5Y-NWXT739-TDN9TK1"  # Токен API
 
     # Заголовки для API-запросов
     API_HEADERS = {
@@ -18,5 +18,4 @@ class Config:
     # Путь к исполняемому файлу ChromeDriver.
     # Используется для автоматизации браузера через Selenium.
     # Значение None означает, что путь пока не определен.
-    CHROME_DRIVER_PATH = None 
-
+    CHROME_DRIVER_PATH = None
